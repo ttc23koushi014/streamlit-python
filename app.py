9d65b14eed635c37cf20fe5f0b876dfc382c7759
Kentaro Taguchi <kentaro@primeplus.local>
2024-07-01 16:17:13 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import yfinance as yf
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+df = yf.download("AAPL", start="2010-11-01", end="2020-11-01")
+st.line_chart(df)
